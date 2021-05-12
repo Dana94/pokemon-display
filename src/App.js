2c81241d4d857686ff0e4a3637fae9668d66f4b0
Dana <dana.ottaviani@gmail.com>
2021-05-12 10:40:38 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 import './App.css';
@@ -8,6 +9,7 @@ function App() {
       {/* <header className="App-header"> */}
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       {/* </header> */}
+      <Header />
       <Main />
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <input type="input" />
+            <nav>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
