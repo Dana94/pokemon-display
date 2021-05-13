93961f42407ba5e446c487e75e92e76f780e5766
Dana <dana.ottaviani@gmail.com>
2021-05-13 09:40:54 -0400

    WIP

--- src/components/Main/Profile/Profile.js
+++ src/components/Main/Profile/Profile.js
@@ -13,7 +13,7 @@ const Profile = () => {
         <div className="Profile">
             <div className="Info">
                 <h1>Name</h1>
-                <p>{dataContext.pokemon.num}</p>
+                {/* <p>{dataContext.pokemon.num}</p> */}
                 <p>develop trick doctor blow clothes hearing flew record dear center friendly brick should happen search page sell current season spring tank color signal disappear</p>
                 <p>Sex Ratio:</p>
             </div>
--- src/context/data-context.js
+++ src/context/data-context.js
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
-import { useQuery, gql } from '@apollo/client';
+// import { useQuery, gql } from '@apollo/client';
 
-const POKEMON_QUERY = gql`
-    query getPokemon {
-        getPokemonDetailsByName(pokemon: eevee reverse: true take: 1) {
-            num
-            sprite
-        }
-    }
-`;
+// const POKEMON_QUERY = gql`
+//     query getPokemon {
+//         getPokemonDetailsByName(pokemon: eevee reverse: true take: 1) {
+//             num
+//             sprite
+//         }
+//     }
+// `;
 
 export const DataContext = React.createContext({
-    pokemon: { name: 'Eevee' },
+    pokemon: {},
+    isLoading: false
 });
 
 const DataContextProvider = props => {
-    const [pokemon, setPokemon] = useState({ name: 'Eevee' });
+    const [pokemon, setPokemon] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
-    const { loading, error, data } = useQuery(POKEMON_QUERY);
+    // const { loading, error, data } = useQuery(POKEMON_QUERY);
 
-    if (loading) return <DataContext.Provider value={{ pokemon: pokemon }}><p>Loading...</p></DataContext.Provider>;
-    if (error) return <DataContext.Provider value={{ pokemon: pokemon }}><p>Error :(</p></DataContext.Provider>;
-    if(data) {
-        setPokemon(data.getPokemonDetailsByName)
-    }
+    // const setPokemonHandler = pokemon => {
+    //     setPokemon(pokemon);
+    // }
 
+    // if (loading) {
+    //     console.log(loading)
+        // setIsLoading(loading);
+    // }
+    // if (error) return <DataContext.Provider value={{ pokemon: pokemon }}><p>Error :(</p></DataContext.Provider>;
+    // if (data) {
+    //     console.log(data)
+    //     setPokemonHandler(data.getPokemonDetailsByName)
+    // }
     return (
-        <DataContext.Provider value={{ pokemon: pokemon }}>
+        <DataContext.Provider value={{ pokemon: pokemon, loading: loading }}>
             {props.children}
         </DataContext.Provider>
     )
+
 }
 
 export default DataContextProvider;
